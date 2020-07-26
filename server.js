21b90c66772ba580a57b8e59aee51dccf091c86c
Praveen Kumar <praveenscience@gmail.com>
2020-07-26 14:44:44 +0100

    Basic hello world REST API.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("This is Server JS.");
+// Import Express!
+const express = require("express");
+// Create an instance.
+const app = express();
+// Define a port.
+const port = 3000;
+
+// Define routes!
+app.get("/", (req, res) => {
+  res.json(0.1 + 0.2);
+});
+
+// Listen to the port.
+app.listen(port, () => {
+  console.log(`Server started in port ${port}.`);
+});
